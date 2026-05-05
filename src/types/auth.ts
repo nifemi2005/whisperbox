fd011491135091c77c6440475240911311801a37
Temenu Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2026-05-05 10:28:27 +0100

    Add TypeScript types for User, Session, RegisterPayload, and LoginPayload

---
+++ src/types/auth.ts
@@ -0,0 +1,32 @@
+export type User = {
+  id: string;
+  usernmae: string;
+  display_name: string;
+  public_key: string;
+  wrapped_private_key: string;
+  pbkdf2_salt: string;
+  created_at: string;
+};
+
+export type Session = {
+  access_token: string;
+  refresh_token: string;
+  token_type: string;
+  expires_in: number;
+  user: User;
+};
+
+export type RegisterPayload = {
+  username: string;
+  display_name: string;
+  password: string;
+  public_key: string;
+  wrapped_private_key: string;
+  pbkdf2_salt: string;
+};
+
+export type LoginPayLoad = {
+  username: string;
+  password: string;
+};
+
